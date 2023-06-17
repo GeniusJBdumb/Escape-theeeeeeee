876d6a735c2c8a4db8d3587048810c0d6c88fcf6
GeniusJBdumb <130238637+GeniusJBdumb@users.noreply.github.com>
2023-06-17 13:39:54 +0200

    pause menu
    added pause menu with function to go back to main menu and restart basically and quit game and resume game. time gets stoped if opausing game. added onFoot function in input system and by pressing "P" game gets paused. resumed can be with resume button. applied pause menu script to player and added input manager component in pausemenu script. In script and game still code and pauseCube since wanted initially to have if interaction with cube that pause, but did not figuered out how to make it work.

---
+++ Main and options escape thee Test/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,98 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement; // for switching between scenes
+
+public class PauseMenu : MonoBehaviour
+{
+    public GameObject pauseMenu; // reference for menu that got created
+    public static bool isPaused; // to see if game is paused already
+    //public KeyCode pauseKey;
+    //public bool hitCube;
+    private InputManager inputManager;// reference to input manager
+
+    void Start()
+    {
+        pauseMenu.SetActive(false); //initially the game is not paused
+       // hitCube = false;
+        inputManager = GetComponent<InputManager>(); // assign Inputmanager
+        isPaused = false;
+
+    }
+    
+    void Update()
+    {
+        // check if player presses pause button
+        //if(//the cube is hit interaction stuff)
+        //{
+        //if (hitCube == true)
+        if(inputManager.onFoot.Pause.triggered)
+        {    
+            Debug.Log("hit P Key");
+            // fi the game is paused
+            if(isPaused)
+            {
+                ResumeGame(); // start the gab´me again
+            }
+            else
+            {
+                PauseGame(); //stop the game
+            }
+        }
+    }
+
+    // if the game gets paused
+    public void PauseGame()
+    {
+        // we activate the pause menu panel
+        pauseMenu.SetActive(true);
+        // for pausing stop ingame clock -> zero
+        Time.timeScale = 0f;
+        isPaused = true;
+     //   hitCube = true;
+    }
+
+    // if the game gets resumed
+    public void ResumeGame()
+    {
+        // do opposite than in PauseGame
+        // we deactivate the pause menu panel
+        pauseMenu.SetActive(false);
+        // for reuming start ingame clock again -> 1
+        Time.timeScale = 1f;
+        isPaused = false;
+       // hitCube = false;
+
+    }
+
+    //public void HitCube()
+    //{
+     //   if(isPaused == false)
+       // {
+        //    hitCube = true;
+       // }
+
+
+   // }
+    // if main Menu gets pressed start game
+    public void GoToMainMenu()
+    {
+        // we deactivate the pause menu panel
+        pauseMenu.SetActive(false);
+        // for going back to start -> start ingame clock again -> 1
+        Time.timeScale = 1f; 
+
+        // have Main as Scene 0 in build Index and Game in Index 1 --> current Index - 1 
+        // other option in loadScene("name of scene")
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+
+    }
+
+    // if Quit gets pressed end game
+    public void Quit()
+    {
+        Application.Quit();
+        //add debug message
+        Debug.Log("PLayer Quit game");
+    }
+}
